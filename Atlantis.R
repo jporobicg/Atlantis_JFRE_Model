3d53201ca4d681ad24f55ad03c501cf0d5441d41
Javier Porobic <jporobicg@gmail.com>
2015-07-13 16:47:45 +1000

    first commit

---
+++ Atlantis.R
@@ -0,0 +1,8 @@
+## library(devtools)
+## install_github(repo = "cddesja/rlantis")
+
+
+
+
+testdata <- gather_data_for_species (species_list_location = "C:/Users/SGosnell/Desktop/bgm",
+                         species_list_csv = "species_test.csv")
