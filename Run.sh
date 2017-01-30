0ae2b72d50aa675a5ba08793a39543a04e35d91e
jporobicg <jporobicg@gmail.com>
2017-01-30 12:22:57 +1100

    Automatic backup 20170130122255

--- Run.sh
+++ Run.sh
@@ -35,7 +35,7 @@ echo "  - OS : $osver">> RunTrack.org
 echo "  - Compiler version : gcc $(gcc -dumpversion)">> RunTrack.org
 echo "  - Bash Version : $BASH_VERSION">> RunTrack.org
 echo "  - Atlantis Version : $(sed -n '2p' < log.txt)">> RunTrack.org
-echo "  - Run used :  $(sed -n '14p' < log.txt)"
+echo "  - Run used :  $(sed -n '14p' < log.txt)">> RunTrack.org
 
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo " Do you want and automatic commit? (Y)ES or (N)OT and press [ENTER]"
