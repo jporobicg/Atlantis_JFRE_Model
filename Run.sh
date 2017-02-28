f1ba5721f61dba7c4bdb395ce0628929aa1c90ca
jporobicg <jporobicg@gmail.com>
2017-02-28 16:39:07 +1100

    changed output location\nMovement of lobster and Crab to sedentary\n- No movement\nThe model is using the distgribution that I put on the .prm file (this is nor dinamyc)
    Former-commit-id: d37c732ee4f6e22835959332488099e638f0c3f1 [formerly bc9527dca1afd28463d71c107000aafce91dfcc1]
    Former-commit-id: 872e2fbd59f2b6593c389e83e32473de82abfb64

--- Run.sh
+++ Run.sh
@@ -10,7 +10,7 @@ dateinicio="$(date +'%Y-%m-%d %H:%M:%S')"
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
 ## ~           Running Atlantis JFRE        ~ ##
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
-gdb --args atlantisMerged -i JFRE.ini.nc 0 -o outputJFRE.nc -r JFRERun.prm -f JFREForcing.prm -p JFREPhysics.prm -b JFREBiol.prm  -s JFREGroups.csv
+gdb --args atlantisMerged -i JFRE.ini.nc 0 -o outputJFRE.nc -r JFRERun.prm -f JFREForcing.prm -p JFREPhysics.prm -b JFREBiol.prm  -s JFREGroups.csv -d JFR_Output_Folder
 
 # ## Backup of the log.txt file
 # echo "++++++++++++++++++++++++++++++++++++++++"
